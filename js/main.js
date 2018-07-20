ada61fdf2ed6e02456a7aa395045c23b3ebe3f2a
Ryan Clark <35505810+QualiaRC@users.noreply.github.com>
2018-07-19 21:22:30 -0500

    Adding everything

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+	"use strict";
+
+	$("#key-c").mousedown(function() {
+		$("#note-c")[0].currentTime = 0;
+		$("#note-c")[0].play();
+	});
+	
+	$("#key-d").mousedown(function() {
+		$("#note-d")[0].currentTime = 0;
+		$("#note-d")[0].play();
+	});
+	
+	$("#key-e").mousedown(function() {
+		$("#note-e")[0].currentTime = 0;
+		$("#note-e")[0].play();
+	});
+	
+	$("#key-f").mousedown(function() {
+		$("#note-f")[0].currentTime = 0;
+		$("#note-f")[0].play();
+	});
+	
+	$("#key-g").mousedown(function() {
+		$("#note-g")[0].currentTime = 0;
+		$("#note-g")[0].play();
+	});
+	
+	$("#key-a").mousedown(function() {
+		$("#note-a")[0].currentTime = 0;
+		$("#note-a")[0].play();
+	});
+	
+	$("#key-b").mousedown(function() {
+		$("#note-b")[0].currentTime = 0;
+		$("#note-b")[0].play();
+	});
+	
+	$("#key-db").mousedown(function() {
+		$("#note-db")[0].currentTime = 0;
+		$("#note-db")[0].play();
+	});
+	
+	$("#key-eb").mousedown(function() {
+		$("#note-eb")[0].currentTime = 0;
+		$("#note-eb")[0].play();
+	});
+	
+	$("#key-gb").mousedown(function() {
+		$("#note-gb")[0].currentTime = 0;
+		$("#note-gb")[0].play();
+	});
+	
+	$("#key-ab").mousedown(function() {
+		$("#note-ab")[0].currentTime = 0;
+		$("#note-ab")[0].play();
+	});
+	
+	$("#key-bb").mousedown(function() {
+		$("#note-bb")[0].currentTime = 0;
+		$("#note-bb")[0].play();
+	});
+
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Boring piano.
+It's just a piano. You can access it here: https://qualiarc.github.io/boring-piano/
+
+# Credits
+ - .mp3s [here](https://github.com/pffy/mp3-piano-sound).
